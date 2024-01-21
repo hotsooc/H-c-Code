299357559961d748a4245a0b91ea5f489c9e5a63
Your Name <you@example.com>
2024-01-21 21:28:57 +0700

    first commit

---
+++ js/data.js
@@ -0,0 +1,26 @@
+let data = [
+    {
+        name : 'Lap trinh web can ban',
+        completed: true,
+    },
+    {
+        name : 'Lap trinh Fullstack Web',
+        completed: false,
+    },
+    {
+        name : 'Lap trinh Mobile app',
+        completed: false,
+    },
+    {
+        name : 'Lap trinh Nodejs',
+        completed: false,
+    },
+    {
+        name : 'Reactjs',
+        completed: false,
+    },
+    {
+        name : 'PHP',
+        completed: false,
+    } 
+]
